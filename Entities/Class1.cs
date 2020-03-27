e23f5ec5f3779c02518465c1450b644d6e72ed58
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:02:02 +0530

    files added

---
+++ Entities/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+   public  class Class1
+    {
+        //Create tables for Person and Person Phone in the ASP .Net application. Define one to many relationship between tables using Entity Framework Core.
+    }
+}
